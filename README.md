5bcbec69279a8d075aa5d707ea615421f0fefa45
Deron-2009 <mullailaw7@gmail.com>
2024-05-29 13:29:05 +0400

    Hackathon24Nasheed

---
+++ README.md
@@ -0,0 +1,10 @@
+## Deron Lawrance
+### This website is made using HTML, CSS and JavaScript. 
+### Find out more about Sustainability and its importance.
+
+### Want to see my webpage???
+Click [here](https://deron-2009.github.io/Sustainability-final/)
+
+## Thank you!!! :)
+
+![Image](/img/whatimg.png)
---
+++ script2.js
@@ -0,0 +1,27 @@
+const calculatorForm = document.querySelector("#carbonCalculator");
+const resultp = document.querySelector("#resultp");
+const finalsubmit = document.querySelector("#finalsubmit")
+
+
+finalsubmit.addEventListener("click", function() {
+    const electricity = parseFloat(document.querySelector("#electricity").value);
+    const transport = parseFloat(document.querySelector("#transport").value);
+    const waste = parseFloat(document.querySelector("#waste").value);
+    const electricityEmissionFactor = 0.5; // kg CO2 per kWh
+    const transportEmissionFactor = 0.2; // kg CO2 per mile
+    const wasteEmissionFactor = 0.1; // kg CO2 per pound
+
+    const electricityEmissions = electricity * electricityEmissionFactor;
+    const transportEmissions = transport * transportEmissionFactor;
+    const wasteEmissions = waste * wasteEmissionFactor;
+
+    const totalEmissions = electricityEmissions + transportEmissions + wasteEmissions;  
+    if(totalEmissions > 2){
+        console.log("Good!");
+        resultp.textContent = "You will have to improve on your energy consumption.";
+    } 
+    else {
+        console.log("Good!");
+        resultp.textContent = "You are doing well with your energy"
+    }
+})
